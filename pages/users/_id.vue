f2b37e1718b9492776b3ddc9586f93dc272a59c0
kakudooo <kakudou3@gmail.com>
2018-12-24 18:00:37 +0900

    html meta

--- pages/users/_id.vue
+++ pages/users/_id.vue
@@ -24,6 +24,11 @@
 </template>
 <script>
 export default {
+  head() {
+    return {
+      title: this.user.id
+    }
+  },
   async asyncData({ route, app }) {
     const user = await app.$axios.$get(`https://qiita.com/api/v2/users/${route.params.id}`)
     const items = await app.$axios.$get(`https://qiita.com/api/v2/items?query=user:${route.params.id}`)
